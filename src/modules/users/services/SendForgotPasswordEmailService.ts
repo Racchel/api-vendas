d0a68b8e6836600f59026b4223f22fd688908e0b
Racchel Velasco <racchelvelasco@gmail.com>
2021-08-06 03:33:38 -0300

    Template Handlebars para email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,34 +1,38 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import UserRepository from '../typeorm/repositories/UserRepository';
+import UsersRepository from '../typeorm/repositories/UserRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
-
 interface IRequest {
   email: string;
 }
-
 class SendForgotPasswordEmailService {
   public async execute({ email }: IRequest): Promise<void> {
-    const usersRepository = getCustomRepository(UserRepository);
+    const usersRepository = getCustomRepository(UsersRepository);
     const userTokensRepository = getCustomRepository(UserTokensRepository);
-
     const user = await usersRepository.findByEmail(email);
-
     if (!user) {
       throw new AppError('User does not exists.');
     }
-    console.log(user);
 
     const { token } = await userTokensRepository.generate(user.id);
 
     //console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
     });
   }
 }
-
 export default SendForgotPasswordEmailService;
